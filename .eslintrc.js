6b2eef0f6e4e65c0f58c77a28ff254ba6a470e75
chenbingchang(陈柄昌) <940001047@qq.com>
2019-12-31 20:32:46 +0800

    初始化项目

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,12 +1,15 @@
 module.exports = {
   root: true,
   env: {
+    browser: true, // 自己加的
     node: true
   },
-  extends: ["plugin:vue/essential", "@vue/prettier"],
+  // 限制vue的风格
+  extends: ["plugin:vue/essential", "@vue/prettier", "standard"],
   rules: {
     "no-console": process.env.NODE_ENV === "production" ? "error" : "off",
-    "no-debugger": process.env.NODE_ENV === "production" ? "error" : "off"
+    "no-debugger": process.env.NODE_ENV === "production" ? "error" : "off",
+    quotes: ['error', "single"]
   },
   parserOptions: {
     parser: "babel-eslint"
--- README.md
+++ README.md
@@ -22,3 +22,7 @@ npm run lint
 
 ### Customize configuration
 See [Configuration Reference](https://cli.vuejs.org/config/).
+
+
+
+项目VSCODE配置：在项目根目录下面创建.vscode文件夹，下面新增settings.json文件，
